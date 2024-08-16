3c5f2853c1c84e8dabd208ed6ac8e05c17c4c554
My Tuyen Phan <mytuyenp@gmail.com>
2024-08-16 08:10:42 -0500

    created button element that acts as squares on board"

--- assignments/react-game-tictactoe/src/components/Board.js
+++ assignments/react-game-tictactoe/src/components/Board.js
@@ -3,7 +3,19 @@ import { useState } from 'react';
 import './Board.css';
 
 function Board() {
-
+  return (
+  <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </>
+);
 };
 
 export default Board;
--- assignments/react-game-tictactoe/src/components/Square.js
+++ assignments/react-game-tictactoe/src/components/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Square.css';
 
 function Square() {
-
+  return <button className="square">X</button>;
 };
 
 export default Square;
