2b18c6c71896cff0c86912e7314650498e16b6bf
My Tuyen Phan <mytuyenp@gmail.com>
2024-08-23 12:44:30 -0500

    added all books data and updated dune isbn

--- assignments/rest-api-project/books.sql
+++ assignments/rest-api-project/books.sql
@@ -1,4 +1,4 @@
- psql library --opens database
+ --psql library --opens database
  --psql database_name -f books.sql --exxecutes sql file
 
 -- SELECT * FROM pg_catalog.pg_tables;
@@ -16,6 +16,17 @@
 --    drop column auhor_last; 
 --    rename column auhor_first to author;
 
-INSERT INTO books
-  VALUES
-    ('0399128964', 'Dune', 'Frank Herbert', null, true);
+-- INSERT INTO books
+--   VALUES
+--     ('9780441013593', 'Dune', 'Frank Herbert', null, true);
+--     ('9780071797580', 'Read & Think Spanish', 'McGraw-Hill', '2024-02-22', false),
+--     ('9781250772909', 'Permanent Record', 'Edward Snowden', null, true),
+--     ('9780060731335', 'Freakonomics: A rouge Economist Explores the Hidden Side of Everything', 'Steven D. Levitt', null, true),
+--     ('9780593685648', 'Crying in H Mart', 'Michelle Zauner', null, true),
+--     ('9780735211292', 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones', 'James Clear', null, true),
+--     ('9780385348119', 'Fluent Forever: How to Learn Any Language Fast and never Forget It', 'Gabriel Wyner', null, true),
+--     ('9780441005901', 'Dune', 'Frank Herbert', '2024-08-22', true);
+
+-- UPDATE books
+-- SET isbn = '9780441013593'
+-- WHERE isbn = '0399128964';
