b34c91db67101bed03a17e6232d4649b977b1ba6
My Tuyen Phan <mytuyenp@gmail.com>
2024-08-23 12:21:18 -0500

    added one row to books table

--- assignments/rest-api-project/books.sql
+++ assignments/rest-api-project/books.sql
@@ -1,19 +1,21 @@
--- psql database_name //opens database
--- psql database_name -f books.sql //exxecutes sql file
+ psql library --opens database
+ --psql database_name -f books.sql --exxecutes sql file
 
 -- SELECT * FROM pg_catalog.pg_tables;
 
--- create table books (
---   isbn char(13),
---   title varchar(150),
---   auhor_first varchar (50),
---   auhor_last varchar (50),
---   last_reserved_at date,
---   is_available boolean
--- );
+--  create table books (
+--    isbn char(13),
+--    title varchar(150),
+--    auhor_first varchar (50),
+--    auhor_last varchar (50),
+--    last_reserved_at date,
+--    is_available boolean
+--  );
 
--- alter table books
-  -- drop column auhor_last; 
-  -- rename column auhor_first to author;
+--  alter table books
+--    drop column auhor_last; 
+--    rename column auhor_first to author;
 
-
+INSERT INTO books
+  VALUES
+    ('0399128964', 'Dune', 'Frank Herbert', null, true);
