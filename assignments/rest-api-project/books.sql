641bab8f6e09c6a9b9356f73b501f89861a49de3
My Tuyen Phan <mytuyenp@gmail.com>
2024-08-23 11:15:14 -0500

    created books table and columns

---
+++ assignments/rest-api-project/books.sql
@@ -0,0 +1,19 @@
+-- psql database_name //opens database
+-- psql database_name -f books.sql //exxecutes sql file
+
+-- SELECT * FROM pg_catalog.pg_tables;
+
+-- create table books (
+--   isbn char(13),
+--   title varchar(150),
+--   auhor_first varchar (50),
+--   auhor_last varchar (50),
+--   last_reserved_at date,
+--   is_available boolean
+-- );
+
+-- alter table books
+  -- drop column auhor_last; 
+  -- rename column auhor_first to author;
+
+
