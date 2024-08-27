94dd5090bd564197d27e34d2d4605f7bec7468f8
My Tuyen Phan <mytuyenp@gmail.com>
2024-08-27 12:44:53 -0500

    added all code challenge instructions, completed monday

---
+++ code-challenges/week7.js
@@ -0,0 +1,40 @@
+/* TUESDAY */
+
+//Write a JavaScript function that takes two numbers as input and returns the larger number. 
+//Use the ternary operator (condition? valueIfTrue : valueIfFalse) for this task.
+
+function findLargerNumber(a, b) {
+  // condition1 ? (condition2 ? Expression1 : Expression2) : Expression3;
+  return a !== b ? (a > b ? a : b) : "Numbers are equal";
+}
+
+console.log(findLargerNumber(1, 3));
+console.log(findLargerNumber(9, 0));
+console.log(findLargerNumber(0, 0));
+
+
+
+/* WEDNESDAY */
+//Write a JavaScript function that takes two arrays as input 
+//and returns a new array that contains all elements from both input arrays. 
+//Use the spread operator (...) to achieve this.
+
+function combineArrays() {
+
+}
+
+console.log(combineArrays());
+
+
+
+/* THURSDAY */
+//Write a JavaScript function that takes an array of numbers as input
+//& returns a new array with all duplicates removed
+//Use the Set object to achieve this.
+
+function removeDuplicates() {
+
+}
+
+console.log(removeDuplicates());
+
