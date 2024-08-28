123eb794b1604071cfd9f4c9aefc0a58f222463a
My Tuyen Phan <mytuyenp@gmail.com>
2024-08-28 15:02:06 -0500

    completed wednesday code challenge

--- code-challenges/week7.js
+++ code-challenges/week7.js
@@ -1,4 +1,4 @@
-/* TUESDAY */
+/* TUESDAY 
 
 //Write a JavaScript function that takes two numbers as input and returns the larger number. 
 //Use the ternary operator (condition? valueIfTrue : valueIfFalse) for this task.
@@ -11,7 +11,7 @@ function findLargerNumber(a, b) {
 console.log(findLargerNumber(1, 3));
 console.log(findLargerNumber(9, 0));
 console.log(findLargerNumber(0, 0));
-
+*/
 
 
 /* WEDNESDAY */
@@ -19,15 +19,22 @@ console.log(findLargerNumber(0, 0));
 //and returns a new array that contains all elements from both input arrays. 
 //Use the spread operator (...) to achieve this.
 
-function combineArrays() {
-
+function combineArrays(arr1, arr2) {
+  const arr3 = []; // initialize arr3
+  // add all arr1 elements to arr3
+  arr3.push(...arr1);
+  // add all arr2 elements to arr3\
+  arr3.push(...arr2);
+  return arr3;
 }
 
-console.log(combineArrays());
+numArr = [1, 2, 3];
+strArr = [];
+console.log(combineArrays(strArr, numArr));
 
 
 
-/* THURSDAY */
+/* THURSDAY 
 //Write a JavaScript function that takes an array of numbers as input
 //& returns a new array with all duplicates removed
 //Use the Set object to achieve this.
@@ -37,3 +44,4 @@ function removeDuplicates() {
 }
 
 console.log(removeDuplicates());
+*/
