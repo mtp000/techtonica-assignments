3517f4f00b93254c54b490bf41b9d6d07f5a076e
My Tuyen Phan <mytuyenp@gmail.com>
2024-09-05 11:55:40 -0500

    added city state and onChange function to set city state based on change in input fields

--- assignments/weather-api/client/src/components/weatherForm.jsx
+++ assignments/weather-api/client/src/components/weatherForm.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
 function WeatherForm(props) {
+  const [city, setCity] = useState('');
+  
+  const onChange = (e) => {
+    setCity(ev.target.value);
+  }
+
+
   return <div className="weather">"weather"</div>;
 }
 
