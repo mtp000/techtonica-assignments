4a534b43bc990f7f2bc4efc91ab0b8dd2191f0a1
My Tuyen Phan <mytuyenp@gmail.com>
2024-09-06 14:43:49 -0500

    added new questions to route for hardcoded data

--- assignments/trivia-game/server/fakedata.js
+++ assignments/trivia-game/server/fakedata.js
@@ -1,17 +1,90 @@
 // File to save one response for the API so I can use it to hardcode if I need it
 // and don't saturate the API call 
 
-const fakedata = {"response_code":0,
-"results":[{"category":"General Knowledge","type":"boolean","difficulty":"medium",
-"question":"Furby was released in 1998.","correct_answer":"True","incorrect_answers":["False"]},
-{"category":"General Knowledge","type":"boolean","difficulty":"medium",
-"question":"There are 86400 seconds in a day.","correct_answer":"True","incorrect_answers":["False"]},
-{"category":"General Knowledge","type":"boolean","difficulty":"medium",
-"question":"Albert Einstein had trouble with mathematics when he was in school.","correct_answer":"False","incorrect_answers":["True"]},
-{"category":"General Knowledge","type":"boolean","difficulty":"medium",
-"question":"Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.","correct_answer":"True","incorrect_answers":["False"]},
-{"category":"General Knowledge","type":"boolean","difficulty":"medium",
-"question":"The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.","correct_answer":"False","incorrect_answers":["True"]
-}]}
+const fakedata = {
+  response_code: 0,
+  results: [
+    {
+      type: 'boolean',
+      difficulty: 'easy',
+      category: 'Entertainment: Video Games',
+      question: 'There are 6 legendary cards in &quot;Clash Royale&quot;.',
+      correct_answer: 'False',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'medium',
+      category: 'Entertainment: Cartoon &amp; Animations',
+      question: 'Donald Duck played the role of Bob Cratchit in Disney&#039;s 1983 adaptation of A Christmas Carol.',
+      correct_answer: 'False',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'easy',
+      category: 'General Knowledge',
+      question: 'When you cry in space, your tears stick to your face.',
+      correct_answer: 'True',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'easy',
+      category: 'Science &amp; Nature',
+      question: 'Salt is 100% composed of Sodium.',
+      correct_answer: 'False',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'medium',
+      category: 'Science &amp; Nature',
+      question: 'Shrimp can swim backwards.',
+      correct_answer: 'True',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'easy',
+      category: 'History',
+      question: 'The United States of America declared their independence from the British Empire on July 4th, 1776.',
+      correct_answer: 'True',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'hard',
+      category: 'Entertainment: Television',
+      question: 'The Klingon home planet is Qo&#039;noS.',
+      correct_answer: 'True',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'medium',
+      category: 'Entertainment: Cartoon &amp; Animations',
+      question: 'In &quot;Avatar: The Last Airbender&quot; and &quot;The Legend of Korra&quot;, Lavabending is a specialized bending technique of Firebending.',
+      correct_answer: 'False',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'easy',
+      category: 'Entertainment: Musicals &amp; Theatres',
+      question: 'IMAX stands for Image Maximum.',
+      correct_answer: 'True',
+      incorrect_answers: [Array]
+    },
+    {
+      type: 'boolean',
+      difficulty: 'medium',
+      category: 'Entertainment: Video Games',
+      question: 'In World of Warcraft lore, Thrall is the original wielder of &quot;Doomhammer&quot;.',
+      correct_answer: 'False',
+      incorrect_answers: [Array]
+    }
+  ]
+}
 
 export default fakedata;
