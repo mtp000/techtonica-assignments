a16751418244131e9b0032baf6d758fdb620b404
My Tuyen Phan <mytuyenp@gmail.com>
2024-09-09 12:15:04 -0500

    initial commit

---
+++ assignments/EventonicaTemplateWithVite/client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Events from './components/events';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Techtonica 2023 H2 events</h1>
+  <Events />
+  </div>
+
+  )
+  
+}
+
+export default App
