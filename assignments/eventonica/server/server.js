043596279fa5b47f79c7158bcf7ecc44d9673366
My Tuyen Phan <mytuyenp@gmail.com>
2024-09-12 15:09:21 -0500

    refactoring for ES modules compatibility

--- assignments/eventonica/server/server.js
+++ assignments/eventonica/server/server.js
@@ -3,10 +3,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
-import { Pool } from 'pg';
+import pg from 'pg';
 
-require('dotenv').config();
-const db = require('./db/db-connection.js'); 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import db from './db/db-connection.js';
 
 const app = express();
 const PORT = 8080;
@@ -26,33 +28,33 @@ app.get("/", (req, res) => {
 
 app.get('/api/events', async (req, res) =>{
 
-    // //real connection with the DB eventonica
-    // try{
-    //     const { rows: events } = await db.query('SELECT * FROM events');
-    //     res.send(events);
-
-    // } catch(error){
-    //     console.log(error);
-    //     return res.status(400).json({error});
-
-    //}
-
-    //hardcode the events response for testing reasons. This call has one more event that the real DB 
+    //real connection with the DB eventonica
     try{
-        const events = [
-
-            {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
-            {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
-            {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
-            {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
-            {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
-            {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
-          ];
-        res.json(events);
+        const { rows: events } = await db.query('SELECT * FROM events');
+        res.send(events);
 
     } catch(error){
         console.log(error);
-    }   
+        return res.status(400).json({error});
+
+    }
+
+    //hardcode the events response for testing reasons. This call has one more event that the real DB 
+    // try{
+    //     const events = [
+
+    //         {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
+    //         {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
+    //         {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
+    //         {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
+    //         {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
+    //         {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
+    //       ];
+    //     res.json(events);
+
+    // } catch(error){
+    //     console.log(error);
+    // }   
     
 })
 
