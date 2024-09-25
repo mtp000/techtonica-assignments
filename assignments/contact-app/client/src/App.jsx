507f3eeedcdd4a444faf2473e558a631c13406af
My Tuyen Phan <mytuyenp@gmail.com>
2024-09-25 18:48:44 -0400

    fixed submodule problem

---
+++ assignments/contact-app/client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Events from './components/events';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Contacts</h1>
+    <Events />
+  </div>
+
+  )
+  
+}
+
+export default App;
