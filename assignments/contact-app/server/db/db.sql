5fd8f87e955a430d6d24bb27e6fcb54bb114b5a3
My Tuyen Phan <mytuyenp@gmail.com>
2024-09-27 12:32:14 -0400

    created database file

---
+++ assignments/contact-app/server/db/db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE contacts;
+
+CREATE TABLE contacts (
+  contact_id serial PRIMARY KEY, 
+  name varchar(150) NOT NULL,
+  email varchar(150),
+  phone varchar(15),
+  notes text
+);
+
+INSERT INTO contacts (name, email, phone, notes)
+ VALUES
+  ('Mom', 'mom@gmail.com', '678-233-2455', 'likes fruits' ),
+  ('Dad', 'dad900@gmail.com', '678-011-0301', 'only texts'),
+  ('Michael', 'michaelp@gmail.com', '678-532-0713', 'eats everything');
